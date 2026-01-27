d6320ab12ef389bc219e4e8310035285df0618df
mohammedaminekamili <mohamedaninekamili2000@gmail.com>
2026-01-27 13:54:55 +0100

    first commit

--- geeks_institute_bootcamp/week_1/day_1/exercises/daily_challenge_not_bad.js
+++
@@ -1,8 +0,0 @@
-let sentence="the movie is not that bad! I like it."
-let wordNot=sentence.indexOf("not")
-let wordBad=sentence.indexOf("bad")
-
-if(wordNot>0 && wordNot<wordBad){
-    sentence=sentence.slice(0,wordNot)+"good"+sentence.slice(wordBad+3)
-}
-console.log(sentence)
