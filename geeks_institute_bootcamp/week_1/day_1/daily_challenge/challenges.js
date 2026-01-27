93ca43e4b2f2df99ada11093e182c9e42390ffb2
mohammedaminekamili <mohamedaninekamili2000@gmail.com>
2026-01-27 13:55:33 +0100

    first commit

--- geeks_institute_bootcamp/week_1/day_1/daily_challenge/challenges.js
+++ geeks_institute_bootcamp/week_1/day_1/daily_challenge/challenges.js
@@ -1,5 +1,24 @@
 // ====== Daily Challenge 1
+let sentence="the movie is not that bad! I like it."
+let wordNot=sentence.indexOf("not")
+let wordBad=sentence.indexOf("bad")
 
+if(wordNot>0 && wordNot<wordBad){
+    sentence=sentence.slice(0,wordNot)+"good"+sentence.slice(wordBad+3)
+}
+console.log(sentence)
 // ====== Daily Challenge 2
+let pattern=""
+for (let i=0;i<6;i++){
+    pattern=pattern+"*"
+   console.log(pattern)
+    
+}
 
-// ====== Daily Challenge 3
+for (let index = 0; index <7; index++) {
+    let row=""
+    for (let j= 0; j< index; j++) {
+        row+="*"
+    }
+    console.log(row)
+}
