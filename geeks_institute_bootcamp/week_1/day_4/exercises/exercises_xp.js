cbfbf25158881704b705d1b768e412270723f568
mohammedaminekamili <mohamedaninekamili2000@gmail.com>
2026-02-01 21:16:25 +0100

    day2

--- geeks_institute_bootcamp/week_1/day_4/exercises/exercises_xp.js
+++ geeks_institute_bootcamp/week_1/day_4/exercises/exercises_xp.js
@@ -1,9 +1,22 @@
 // ===== Exercise 1
-
+function compareToTen(num) {
+    return new Promise((resolve, reject) => {
+        if (num <= 10) {
+            resolve(`${num} is less than or equal to 10. Promise Resolved!`);
+        } else {
+            reject(`${num} is greater than 10. Promise Rejected!`);
+        }
+    });
+}
 // ===== Exercise 2
-
+const delayedSuccess = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve("success");
+    }, 4000); // 4000 milliseconds = 4 seconds
+});
 // ===== Exercise 3
-
+const resolvedPromise = Promise.resolve(3);
+const rejectedPromise = Promise.reject("Boo!");
 // ===== Exercise 4
 
 // ===== Exercise ...
