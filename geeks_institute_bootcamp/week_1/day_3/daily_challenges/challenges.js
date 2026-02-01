59806ca7e6d77c797e323f1be814435cb0c45de7
mohammedaminekamili <mohamedaninekamili2000@gmail.com>
2026-02-01 20:46:12 +0100

    day2

--- geeks_institute_bootcamp/week_1/day_3/daily_challenges/challenges.js
+++ geeks_institute_bootcamp/week_1/day_3/daily_challenges/challenges.js
@@ -1,5 +1,24 @@
 // ====== Daily Challenge 1
+const libForm = document.getElementById("libform");
+const storySpan = document.getElementById("story");
 
-// ====== Daily Challenge 2
 
-// ====== Daily Challenge 3
+libForm.addEventListener("submit", function(event) {    
+    event.preventDefault();
+
+    const noun = document.getElementById("noun").value.trim();
+    const adjective = document.getElementById("adjective").value.trim();
+    const person = document.getElementById("person").value.trim();
+    const verb = document.getElementById("verb").value.trim();
+    const place = document.getElementById("place").value.trim();
+
+    if (noun === "" || adjective === "" || person === "" || verb === "" || place === "") {
+        alert("Please fill in all the blanks before clicking 'Lib it!'");
+        return; 
+    }
+    const story = `One day, ${person} went to ${place} with a very ${adjective} ${noun}. 
+                   Everyone was shocked when they started to ${verb} right in the middle of the street!`;
+
+
+    storySpan.textContent = story;
+});
