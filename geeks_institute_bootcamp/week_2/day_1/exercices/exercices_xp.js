ce70474f90a8d8c6985e7086a03f079613c2c334
mohammedaminekamili <mohamedaninekamili2000@gmail.com>
2026-02-04 10:20:49 +0100

    advanced

--- geeks_institute_bootcamp/week_2/day_1/exercices/exercices_xp.js
+++ geeks_institute_bootcamp/week_2/day_1/exercices/exercices_xp.js
@@ -90,6 +90,65 @@ function toGramme(x){
 return x*1000
 }
 
-const toGramme=function (x){
+let toGrammeEx=function (x){
     return x*1000
 }
+//function expression create anonymous functions and they are called by the name of the variable
+let toGrammeOL=x=>x*1000
+
+//Exercice 6
+
+(function (job, location, partner, childrenNumber) {
+    document.body.textContent =
+      `You will be a ${job} in ${location}, and married to ${partner} with ${childrenNumber} kids.`;
+  })("job", "location", "partner", 3);
+
+  // Exercice 7
+  
+const body =document.body
+const navBar= document.createElement("nav")
+body.appendChild(navBar)
+(function(name){
+    const div=document.createElement("div")
+    navBar.appendChild(div)
+    const img =document.createElement("img")
+    const p= document.createElement("p")
+    p.textContent=name
+    div.appendChild(img)
+    div.appendChild(p)
+})("john")
+
+//Exercice 8
+
+//Part I
+function makeJuice(size) {
+    function addIngredients(ingredient1,ingredient2,ingredient3) {
+        const body=document.body
+        const p = document.createElement("p")
+        p.textContent=`The client wants a ${size} juice, containing ${ingredient1}, ${ingredient2}, ${ingredient3}`
+        body.appendChild(p)
+    }
+    console.log("this is inner execution")
+    addIngredients("mm","nn","nn")
+}
+makeJuice("medium")
+
+//PArtII
+
+function makeJuice(size) {
+    let ingredients=[]
+function addIngredients(ingredient1,ingredient2,ingredient3) {
+ingredients.push(ingredient1, ingredient2, ingredient3)}
+function displayJuice(){
+const body=document.body
+const p = document.createElement("p")
+let message = ingredients.join(", ")
+p.textContent=`The client wants a ${size} juice, containing ${message}`
+body.appendChild(p)
+}
+addIngredients("mm","nn","nn")
+addIngredients("ws","wswsw","dxd")
+displayJuice()
+}
+makeJuice("medium")
+  
