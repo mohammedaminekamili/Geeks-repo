2e794f77912bd6d5f389333901ff1e0d41ef9145
mohammedaminekamili <mohamedaninekamili2000@gmail.com>
2026-02-05 12:35:02 +0100

    day2

--- geeks_institute_bootcamp/week_2/day_3/daily_challenges/daily_challenges.js
+++ geeks_institute_bootcamp/week_2/day_3/daily_challenges/daily_challenges.js
@@ -1,31 +1,6 @@
-class Video{
-    constructor(title,uploader,time){
-        this.title=title
-        this.uploader=uploader
-        this.time=time
-
-    }
-    watch(){
-        console.log(`${this.uploader} watched all ${this.time} of ${this.title}!`)
-    }
-
+function anagram(word1,word2){
+    let comp1= word1.trim().toLowerCase().split(" ").join("").split("").sort().join("")
+        let comp2= word2.trim().toLowerCase().split(" ").join("").split("").sort().join("")
+    
+return comp1==comp2
 }
-
-const firstVid=new Video("tuto","bahaj",15)
-firstVid.watch()
-const secondVid=new Video("song","X",3)
-
-
-const videoData = [
-    { title: "JS Tutorial", uploader: "Ben", time: 300 },
-    { title: "Cooking Pasta", uploader: "Maria", time: 600 },
-    { title: "Workout Routine", uploader: "Alex", time: 1200 },
-    { title: "Cats Being Cats", uploader: "Sarah", time: 60 },
-    { title: "Node.js Basics", uploader: "John", time: 900 }
-];
-
-const videoInstances = videoData.map(data => {
-    return new Video(data.title, data.uploader, data.time);
-});
-
-
