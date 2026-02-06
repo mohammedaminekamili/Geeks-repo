2757bfcc4ca11aa266460c6976926b74cf864417
mohammedaminekamili <mohamedaninekamili2000@gmail.com>
2026-02-06 11:57:08 +0100

    day2

--- geeks_institute_bootcamp/week_1/day_3/exercises/exercises_xp_gold.js
+++ geeks_institute_bootcamp/week_1/day_3/exercises/exercises_xp_gold.js
@@ -1,9 +1,52 @@
 // ===== Exercise 1
-
+const select=document.getElementById("genres")
+console.log(select.value)
+const option= document.createElement("option")
+option.textContent="Classic"
+option.value="classic"
+select.appendChild(option)
+option.setAttribute("selected","true")
 // ===== Exercise 2
+const form=document.forms[0]
+const button=form[1]
+function removecolor(){
+    const options= document.getElementsByTagName("option")
+    for (const option of options) {
+     if (option.selected) {
+        option.remove()
+     }
+        
+    }
+    
 
+}
+button.addEventListener("click",removecolor)
 // ===== Exercise 3
-
+let shoppingList=[]
+const div=document.getElementById("root")
+const form=document.createElement("form")
+const input = document.createElement("input")
+const button= document.createElement("button")
+button.textContent="AddItem"
+input.setAttribute("type","text")
+form.appendChild(input)
+form.appendChild(button)
+div.appendChild(form)
+function addItem(e){
+    e.preventDefault()
+   
+    shoppingList.push(input.value)
+    input.value=""
+}
+button.addEventListener("click",addItem)
+const clear=document.createElement("button")
+clear.textContent="ClearAll"
+form.appendChild(clear)
+const clearAll=(e)=>{
+e.preventDefault()
+shoppingList=[]
+}
+clear.addEventListener("click",clearAll)
 // ===== Exercise 4
 
 // ===== Exercise ...
