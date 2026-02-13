0d820379a64f473fb70aa0ae2d5ed5abded87993
mohammedaminekamili <mohamedaninekamili2000@gmail.com>
2026-02-13 10:17:19 +0100

    day2

---
+++ geeks_institute_bootcamp/week_1/day_5/mini_project_2/sounds/drum.js
@@ -0,0 +1,30 @@
+// const body=document.body()
+let spans=document.querySelectorAll("span")
+
+// spans[0].addEventListner("click",(e)=>{
+// console.log("ra 5dama")
+
+
+    spans.forEach(e => {e.addEventListener("click", ()=>{
+        let id = e.children[1].textContent
+        console.log(id)
+        let audio = document.getElementById(id)
+        audio.play()
+        audio.currentTime =0
+    });
+} )
+spans.forEach(e=>{
+    document.addEventListener("keydown",(event)=>{
+        if (event.key===e.children[0].textContent.toLowerCase()) {
+            let id = e.children[1].textContent
+            
+            let audio = document.getElementById(id)
+            audio.play()
+            audio.currentTime=0
+        }
+        
+
+    })
+})
+
+
