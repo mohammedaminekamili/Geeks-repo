03e870dd1976d26b97c5d9ed9bc7f1c3575d9db1
mohammedaminekamili <mohamedaninekamili2000@gmail.com>
2026-02-24 12:19:42 +0000

    day2

--- geeks_institute_bootcamp/week_2/day_3/daily_challenges/daily_challenges.js
+++ geeks_institute_bootcamp/week_2/day_3/daily_challenges/daily_challenges.js
@@ -1,6 +1,6 @@
 function anagram(word1,word2){
-    let comp1= word1.trim().toLowerCase().split(" ").join("").split("").sort().join("")
-        let comp2= word2.trim().toLowerCase().split(" ").join("").split("").sort().join("")
+    let comp1= word1.toLowerCase().split(" ").join("").split("").sort().join("")
+        let comp2= word2.toLowerCase().split(" ").join("").split("").sort().join("")
     
 return comp1==comp2
 }
