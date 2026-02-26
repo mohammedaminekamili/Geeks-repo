a30b13754b4cd5c7e98b6965249cbf2266b88a06
mohammedaminekamili <mohamedaninekamili2000@gmail.com>
2026-02-26 15:20:47 +0000

    aa

---
+++ geeks_institute_bootcamp/week_3/day_5/exercices_xp/exercice1/data.js
@@ -0,0 +1,35 @@
+export const posts = [
+    {
+      id: 1,
+      title: "Getting Started with Node.js",
+      content: "Node.js lets you run JavaScript on the server. In this post, we'll cover installation and your first script."
+    },
+    {
+      id: 2,
+      title: "Understanding Express Routes",
+      content: "Express makes it easy to define routes for your web application. Let's explore GET, POST, PUT, and DELETE."
+    },
+    {
+      id: 3,
+      title: "Working with Middleware",
+      content: "Middleware functions in Express can log requests, handle errors, and add custom logic between request and response."
+    },
+        {
+          id: 4,
+          title: "Introduction to Express",
+          content: "Express is a web application framework for Node.js.",
+        },
+        {
+          id: 5,
+          title: "Getting Started with APIs",
+          content: "Learn the basics of building APIs with Express.",
+        },
+        {
+          id: 6,
+          title: "Node.js Best Practices",
+          content:
+            "Explore best practices for writing clean and efficient Node.js code.",
+        },
+      ];
+      
+   
---
+++ geeks_institute_bootcamp/week_3/day_5/exercices_xp/exercice1/server.js
@@ -0,0 +1,98 @@
+import express from "express";
+import { posts } from "./data.js";
+
+const data=posts
+      
+  
+  
+const app=express()
+app.use(express.json())
+
+app.get("/posts",(req,res)=>{
+    try {
+        if (data.length<1) {
+            return res.status(404).send("no post found")
+        
+        }
+
+        res.status(200).json(data)
+    } catch (error) {
+        res.status(500).send("server error")
+    }
+})
+
+app.get("/posts/:id",(req,res)=>{
+    try {
+        const id= Number(req.params.id)
+        const post=data.find(element=>element.id===id)
+        if (!post) {
+            return res.status(404).send("no post found")
+        }
+        res.status(200).json(post)
+    } catch (error) {
+        res.status(500).send("server error")
+    }
+})
+
+
+app.post("/posts",(req,res)=>{
+    try {
+        const {title,content}=req.body
+        if (!title || !content) {
+            return removeEventListener.status(400).send("fill all required fields")
+        }
+        const post={
+            id:data.length+1,
+            title,
+            content
+        }
+        data.push(post)
+        res.status(201).json(post)
+
+    } catch (error) {
+        res.status(500).send("server error")
+    }
+})
+
+app.put("/posts/:id",(req,res)=>{
+    try {
+        const id= Number(req.params.id)
+        const index=data.findIndex(element=>element.id===id)
+        if (index==-1) {
+            return res.status(404).send("no post found")
+        }
+        const {title,content}=req.body
+        data[index].content=content
+        data[index].title=title
+        res.status(200).json(data[index])
+
+
+    } catch (error) {
+        res.status(500).send("server error")
+    }
+})
+
+app.delete("/posts/:id",(req,res)=>{
+    try {
+        const id= Number(req.params.id)
+        const index=data.findIndex(element=>element.id===id)
+        if (index==-1) {
+            return res.status(404).send("no post found")
+        }
+       data.splice(index,1)
+        res.status(200).json(data)
+
+
+    } catch (error) {
+        res.status(500).send("server error")
+    }
+})
+
+app.listen(3000,()=>{
+    try {
+        
+        console.log("server working")
+    } catch (error) {
+        console.error(error)
+    }
+})
