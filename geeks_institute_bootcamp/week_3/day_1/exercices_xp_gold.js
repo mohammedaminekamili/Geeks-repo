c85479029b3361931b06d6758bfa39d2921d35d3
mohammedaminekamili <mohamedaninekamili2000@gmail.com>
2026-03-02 09:04:56 +0000

    xp gold

--- geeks_institute_bootcamp/week_3/day_1/exercices_xp_gold.js
+++ geeks_institute_bootcamp/week_3/day_1/exercices_xp_gold.js
@@ -1,3 +1,5 @@
+//Exercice 1
+
 const promise1 = Promise.resolve(3);
 const promise2 = 42; 
 const promise3 = new Promise((resolve, reject) => {
@@ -14,3 +16,6 @@ Promise.all([promise1, promise2, promise3])
   });
   //promise.all is used to handle an array of promises and return an array of results when all promises are resolved or reject if one of the promises is rejected
   //promise.all return the results in the order of the promises in the array 
+
+//Exercice 2
+//[2,4,6]
