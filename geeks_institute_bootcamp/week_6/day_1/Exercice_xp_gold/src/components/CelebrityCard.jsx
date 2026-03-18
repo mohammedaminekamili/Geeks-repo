08b2fd1661b1bf9275f946b3bb3841f12cc336d5
mohammedaminekamili <mohamedaninekamili2000@gmail.com>
2026-03-18 00:21:49 +0000

    react

--- geeks_institute_bootcamp/week_6/day_1/Exercice_xp_gold/src/components/CelebrityCard.jsx
+++ geeks_institute_bootcamp/week_6/day_1/Exercice_xp_gold/src/components/CelebrityCard.jsx
@@ -1,17 +1,19 @@
-function CelebrityCard(celebrity) {
+function CelebrityCard(props) {
     return <div className="max-w-md bg-white shadow rounded p-4">
-    <img src={celebrity.imageUrl} alt="" className="rounded mb-3" />
+    <img src={props.celebrity.imageUrl} alt="" className="rounded mb-3" />
   
     <h2 className="text-xl font-bold mb-2">
-      {celebrity.title}
+      {props.celebrity.title}
     </h2>
   
     <p className="text-gray-600 mb-3">
-      {celebrity.description}
+      {props.celebrity.description}
     </p>
   
-    <a className="bg-blue-500 text-white px-4 py-2 rounded" href={celebrity.buttonUrl}>
-      {celebrity.buttonLabel}
+    <a className="bg-blue-500 text-white px-4 py-2 rounded" href={props.celebrity.buttonUrl}>
+      {props.celebrity.buttonLabel}
     </a>
   </div>
 }
+
+export default CelebrityCard
